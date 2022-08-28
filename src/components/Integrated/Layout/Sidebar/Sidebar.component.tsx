34604890d00d0877b507980c0a4ad95e26cd7a27
jay <contact.jays.space@gmail.com>
2022-08-28 02:14:58 +0200

    Updated Sidebar styles

--- src/components/Integrated/Layout/Sidebar/Sidebar.component.tsx
+++ src/components/Integrated/Layout/Sidebar/Sidebar.component.tsx
@@ -1,14 +1,28 @@
 // CONSTANTS
 import SidebarMenuOptions from "../../../../constants/sidebarOptions.constants";
+import { Text } from "../../../Atomic/Typography/Text";
 
 const Sidebar = () => {
   return (
-    <div className="bg-slate-900 w-1/6">
+    <div className="w-1/6 p-6 bg-slate-900">
+      <div className="mt-4 mb-12 px-4 text-3xl text-white">LOGO</div>
+
       {SidebarMenuOptions.map((optionGroup, idx) => (
-        <div key={idx}>
-          <h4>{optionGroup.groupName}</h4>
+        <div key={idx} className="mb-10">
+          {optionGroup.groupName && (
+            <Text
+              key={idx}
+              className="mb-4 font-semibold uppercase !tracking-widest text-slate-600"
+            >
+              {optionGroup.groupName}
+            </Text>
+          )}
           {optionGroup.optionsList.map((option, idx) => (
-            <div key={idx}>{option.label}</div>
+            <div className="flex items-center mb-2 py-3 px-4 rounded-md cursor-pointer hover:bg-slate-800 transform ease-out duration-100">
+              <Text key={idx} className={"text-lg text-white capitalize"}>
+                {option.label}
+              </Text>
+            </div>
           ))}
         </div>
       ))}
